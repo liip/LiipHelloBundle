0bd75148367a09e36c61bb84b147a2d68892dd10
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-08-18 12:31:01 +0200

    fixed templates

--- Resources/views/Hello/index.html.php
+++ Resources/views/Hello/index.html.php
@@ -1,3 +1,6 @@
 <?php $view->extend('LiipHelloBundle::layout.html.php') ?>
 
+<?php $view['slots']->start('content') ?>
 Hello <?php echo $view->escape($name) ?>!
+<?php $view['slots']->stop() ?>
+
--- Resources/views/layout.html.php
+++ Resources/views/layout.html.php
@@ -1,5 +1,8 @@
 <?php $view->extend('::base.html.php') ?>
 
+<?php $view['slots']->start('body') ?>
 <h1>Hello Application</h1>
 
-<?php $view['slots']->output('_content') ?>
+<?php $view['slots']->output('content') ?>
+
+<?php $view['slots']->stop() ?>
